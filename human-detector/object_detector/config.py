cab460f67bb7edb59938dc3e5684dc28e080d2c6
e.mazurkevich <kiramzit13@gmail.com>
2018-04-24 00:16:13 +0300

    Added config file and extract_features script

---
+++ human-detector/object_detector/config.py
@@ -0,0 +1,14 @@
+pos_im_path = './human-detector/data/images/pos'
+neg_im_path = './human-detector/data/images/neg'
+min_wdw_sz = [68, 124]
+step_size = [10, 10]
+orientations = 9
+pixels_per_cell = [6, 6]
+cells_per_block = [2, 2]
+visualize = False
+normalize = True
+block_normalization = 'L2-Hys'
+pos_feat_ph = './human-detector/data/features/pos'
+neg_feat_ph = './human-detector/data/features/neg'
+model_path = './human-detector/data/models/'
+threshold = .3
---
+++ human-detector/object_detector/extract_features.py
@@ -0,0 +1,33 @@
+from skimage.feature import hog
+from skimage.io import imread
+from sklearn.externals import joblib
+import glob
+import os
+from config import *
+
+def generate_features_and_save_to_file(im_path, feat_dir):
+    im = imread(im_path, as_grey=True)
+    fd = hog(im, orientations, pixels_per_cell, cells_per_block, block_normalization, visualize, normalize)
+    fd_name = os.path.split(im_path)[1].split(".")[0] + ".feat"
+    fd_path = os.path.join(feat_dir, fd_name)
+    joblib.dump(fd, fd_path) 
+
+def extract_features():
+    if not os.path.isdir(pos_feat_ph):
+        os.makedirs(pos_feat_ph)
+
+    if not os.path.isdir(neg_feat_ph):
+        os.makedirs(neg_feat_ph)
+
+    for im_path in glob.glob(os.path.join(pos_im_path, "*")):
+        generate_features_and_save_to_file(im_path, pos_feat_ph)
+    print("Positive features saved in {}".format(pos_feat_ph))
+
+    for im_path in glob.glob(os.path.join(neg_im_path, "*")):
+        generate_features_and_save_to_file(im_path, neg_feat_ph)
+    print("Negative features saved in {}".format(neg_feat_ph))
+
+    print("Completed calculating features from training images")
+
+# if __name__=='__main__':
+extract_features()
